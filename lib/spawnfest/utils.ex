e7b88683436cd3ac4c31ed69cf8fa3da731d2539
Carlo Gilmar <carlogilmar12@gmail.com>
2019-09-21 18:15:09 -0500

    Refactor get_month number function

--- lib/spawnfest/utils.ex
+++ lib/spawnfest/utils.ex
@@ -72,22 +72,21 @@ defmodule UtilDate do
 
   defp get_month_number(month) do
     months =
-      [
-        {"Jan", "01"},
-        {"Feb", "02"},
-        {"Mar", "03"},
-        {"Apr", "04"},
-        {"May", "05"},
-        {"Jun", "06"},
-        {"Jul", "07"},
-        {"Aug", "08"},
-        {"Sep", "09"},
-        {"Oct", "10"},
-        {"Nov", "11"},
-        {"Dec", "12"}
-      ]
-    {_desc, month_number} = Enum.find(months, fn {desc, _month} -> desc == month end)
-    month_number
+      %{
+        "Jan" => "01",
+        "Feb" => "02",
+        "Mar" => "03",
+        "Apr" => "04",
+        "May" => "05",
+        "Jun" => "06",
+        "Jul" => "07",
+        "Aug" => "08",
+        "Sep" => "09",
+        "Oct" => "10",
+        "Nov" => "11",
+        "Dec" => "12"
+      }
+		months[month]
   end
 
 end
