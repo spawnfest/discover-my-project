7bae060a9ae7bcc7d769ecce1f613fd1dc13c2f3
Carlo Gilmar <carlogilmar12@gmail.com>
2019-09-21 23:14:16 -0500

    Adding mexican debuggers

--- lib/spawnfest/core.ex
+++ lib/spawnfest/core.ex
@@ -1,10 +1,14 @@
 defmodule DiscoverMyProject do
 
   def start_analyze(url_repo) do
+    IO.puts "1. Starting..."
     repo_dir = GitEngine.clone_repo(url_repo)
+    IO.puts "2. Repo cloned..."
     branches = GitEngine.get_all_branches(repo_dir)
+    IO.puts "3. Branches done"
     branch = BranchEngine.analize_branch(repo_dir)
-    {branches, branch}
+    IO.puts "4. DONE"
+    {url_repo, branches, branch}
   end
 
   def start_analyze_cloned_repo(repo_dir) do
