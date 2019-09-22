f5df7f5194b677b1b724f1a2688f51cb0f306b04
Carlo Gilmar <carlogilmar12@gmail.com>
2019-09-21 19:14:29 -0500

    Adding day and frequency structs

--- lib/spawnfest/structs.ex
+++ lib/spawnfest/structs.ex
@@ -2,3 +2,43 @@ defmodule Commit do
    defstruct [:hash, :description, :author, :date_created, :words]
 end
 
+defmodule Day do
+	defstruct [
+		hour_1: 0,
+		hour_2: 0,
+		hour_3: 0,
+		hour_4: 0,
+		hour_5: 0,
+		hour_6: 0,
+		hour_7: 0,
+		hour_8: 0,
+		hour_9: 0,
+		hour_10: 0,
+		hour_11: 0,
+		hour_12: 0,
+		hour_13: 0,
+		hour_14: 0,
+		hour_15: 0,
+		hour_16: 0,
+		hour_17: 0,
+		hour_18: 0,
+		hour_19: 0,
+		hour_20: 0,
+		hour_21: 0,
+		hour_22: 0,
+		hour_23: 0,
+		hour_24: 0
+	]
+end
+
+defmodule Frequency do
+	defstruct [
+		monday: %Day{},
+		tuesday: %Day{},
+		wednesday: %Day{},
+		thursday: %Day{},
+		friday: %Day{},
+		saturday: %Day{},
+		sunday: %Day{}]
+end
+
