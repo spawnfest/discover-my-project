c7c7cb8f5731b30cee92858ed9831b9f9056c147
Carlo Gilmar <carlogilmar12@gmail.com>
2019-09-22 02:32:27 -0500

    Analize github data

--- lib/spawnfest/core.ex
+++ lib/spawnfest/core.ex
@@ -7,8 +7,10 @@ defmodule DiscoverMyProject do
     branches = GitEngine.get_all_branches(repo_dir)
     IO.puts "3. Branches done"
     branch = BranchEngine.analize_branch(repo_dir)
-    IO.puts "4. DONE"
-    {url_repo, branches, branch}
+    IO.puts "4. Getting github information"
+    [github_repo, issues, pr, contributors] = GitHubData.get_data_from_github(url_repo)
+    IO.puts "5. DONE!!!"
+    {url_repo, branches, branch, github_repo, issues, pr, contributors}
   end
 
   def start_analyze_cloned_repo(repo_dir) do
