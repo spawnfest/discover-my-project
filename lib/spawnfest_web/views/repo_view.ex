a750f75b05e2b33d2c60f904fbf0b8ab8a1a6337
Carlo Gilmar <carlogilmar12@gmail.com>
2019-09-22 11:22:22 -0500

    Show heat map in view

--- lib/spawnfest_web/views/repo_view.ex
+++ lib/spawnfest_web/views/repo_view.ex
@@ -1,3 +1,18 @@
 defmodule SpawnfestWeb.RepoView do
   use SpawnfestWeb, :view
+
+  def show_heat_map_row(row) do
+    set_class().(row)
+  end
+
+  def set_class do
+    fn
+      0 -> "background-color: #ffffff;"
+      counter when counter in 1..5 -> "background-color: #c1f9e8a3;"
+      counter when counter in 6..10 -> "background-color: #87f5d4;"
+      counter when counter in 10..15 -> "background-color: #00ffb3;"
+      counter when counter in 15..20 -> "background-color: #00c288;"
+      _ -> "background-color: #017351;"
+    end
+  end
 end
