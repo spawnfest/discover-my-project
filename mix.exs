b8ff3c558aa58885af1c36cf6d2bcd4ddd15c527
Carlo Gilmar <carlogilmar12@gmail.com>
2019-09-22 00:18:25 -0500

    Adding  poison and httpoison deps

--- mix.exs
+++ mix.exs
@@ -13,9 +13,6 @@ defmodule Spawnfest.MixProject do
     ]
   end
 
-  # Configuration for the OTP application.
-  #
-  # Type `mix help compile.app` for more information.
   def application do
     [
       mod: {Spawnfest.Application, []},
@@ -23,13 +20,9 @@ defmodule Spawnfest.MixProject do
     ]
   end
 
-  # Specifies which paths to compile per environment.
   defp elixirc_paths(:test), do: ["lib", "test/support"]
   defp elixirc_paths(_), do: ["lib"]
 
-  # Specifies your project dependencies.
-  #
-  # Type `mix help deps` for examples and options.
   defp deps do
     [
       {:phoenix, "~> 1.4.0"},
@@ -39,7 +32,9 @@ defmodule Spawnfest.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
-      {:phoenix_live_view, github:  "phoenixframework/phoenix_live_view"}
+      {:phoenix_live_view, github:  "phoenixframework/phoenix_live_view"},
+      {:poison, "~> 3.1"},
+      {:httpoison, "~> 1.5"}
     ]
   end
 end
