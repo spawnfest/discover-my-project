a13f0f95f4153822bffb7c8a93187271d658e2dc
Carlo Gilmar <carlogilmar12@gmail.com>
2019-09-22 02:08:22 -0500

    Adding task async and task await for get the api responses

--- lib/spawnfest/github_engine.ex
+++ lib/spawnfest/github_engine.ex
@@ -2,22 +2,26 @@ defmodule GitHubApiEngine do
   @api"https://api.github.com/repos/"
   @github_headers [{"Accept", "application/vnd.github.mercy-preview+json"}]
 
-  def get_repo_info({repo, org}) do
-    repo_info = NetworkConsumer.get("#{@api}#{org}/#{repo}", @github_headers)
-    %GitHubRepository{
-      fullname: repo_info["full_name"],
-      avatar_url: repo_info["owner"]["avatar_url"],
-      description: repo_info["description"],
-      open_issues: repo_info["open_issues"],
-      forks: repo_info["forks"],
-      language: repo_info["language"],
-      default_branch: repo_info["default_branch"]
-    }
+  def get_repo_info do
+    fn {repo, org} ->
+      repo_info = NetworkConsumer.get("#{@api}#{org}/#{repo}", @github_headers)
+      %GitHubRepository{
+        fullname: repo_info["full_name"],
+        avatar_url: repo_info["owner"]["avatar_url"],
+        description: repo_info["description"],
+        open_issues: repo_info["open_issues"],
+        forks: repo_info["forks"],
+        language: repo_info["language"],
+        default_branch: repo_info["default_branch"]
+      }
+    end
   end
 
-  def get_repo_issues({repo, org}) do
-    repo_info = NetworkConsumer.get("#{@api}#{org}/#{repo}/issues?state=all", @github_headers)
-    get_issues(repo_info)
+  def get_repo_issues do
+    fn {repo, org} ->
+    	repo_info = NetworkConsumer.get("#{@api}#{org}/#{repo}/issues?state=all", @github_headers)
+    	get_issues(repo_info)
+		end
   end
 
   defp get_issues(repo_info) do
@@ -31,12 +35,14 @@ defmodule GitHubApiEngine do
     end
   end
 
-  def get_repo_pull_requests({repo, org}) do
-    repo_info = NetworkConsumer.get("#{@api}#{org}/#{repo}/pulls?state=all", @github_headers)
-    get_pull_requests(repo_info)
+  def get_repo_pull_requests do
+    fn {repo, org} ->
+    	repo_info = NetworkConsumer.get("#{@api}#{org}/#{repo}/pulls?state=all", @github_headers)
+    	get_pull_requests(repo_info)
+		end
   end
 
-  def get_pull_requests(pulls) do
+  defp get_pull_requests(pulls) do
     for pull <- pulls do
       %GitHubPR{
         number: pull["number"],
@@ -48,9 +54,11 @@ defmodule GitHubApiEngine do
     end
   end
 
-  def get_repo_contributors({repo, org}) do
-    repo_info = NetworkConsumer.get("#{@api}#{org}/#{repo}/contributors", @github_headers)
-    get_contributors(repo_info)
+  def get_repo_contributors do
+    fn {repo, org} ->
+      repo_info = NetworkConsumer.get("#{@api}#{org}/#{repo}/contributors", @github_headers)
+      get_contributors(repo_info)
+		end
   end
 
   def get_contributors(repo_info) do
@@ -72,4 +80,23 @@ defmodule GitHubUtil do
     [repo, ""] = String.split(repo, ".git")
     {repo, org}
   end
+end
+
+defmodule GitHubData do
+
+  def get_data_from_github(url) do
+    repo_and_org = GitHubUtil.get_repo_and_org(url)
+    api_request =
+			[{GitHubApiEngine.get_repo_info, repo_and_org},
+			{GitHubApiEngine.get_repo_issues, repo_and_org},
+			{GitHubApiEngine.get_repo_pull_requests, repo_and_org},
+			{GitHubApiEngine.get_repo_contributors, repo_and_org}]
+		res =
+      api_request
+      |> Enum.map(fn {builder, args} -> apply_execution(builder, args) end)
+      |> Enum.map(fn builder -> Task.async(builder) end)
+      |> Enum.map(fn task -> Task.await(task, 9000) end)
+  end
+
+	defp apply_execution(fun, args), do: fn -> fun.(args) end
 end
