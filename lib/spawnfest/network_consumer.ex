a5f0df467f31779423f00cbb067fac37327c421d
Carlo Gilmar <carlogilmar12@gmail.com>
2019-09-22 01:15:50 -0500

    Adding get func

--- lib/spawnfest/network_consumer.ex
+++ lib/spawnfest/network_consumer.ex
@@ -2,6 +2,15 @@ defmodule NetworkConsumer do
 
   def get(url) do
     response = HTTPoison.get!(url)
+    get_response(response)
+  end
+
+  def get(url, headers) do
+    response = HTTPoison.get!(url, headers)
+    get_response(response)
+  end
+
+  defp get_response(response) do
     {:ok, response_body} = Poison.decode(response.body)
     response_body
   end
